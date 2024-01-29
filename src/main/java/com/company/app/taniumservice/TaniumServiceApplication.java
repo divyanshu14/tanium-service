6a91d444acf6e5ab8518370c431741c087366f8d
Divyanshu <iamavi53@gmail.com>
2024-01-29 13:21:06 +0530

    spring boot 3

---
+++ src/main/java/com/company/app/taniumservice/TaniumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.app.taniumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaniumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaniumServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/app/taniumservice/TaniumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.app.taniumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaniumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
